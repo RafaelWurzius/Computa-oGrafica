6e26b7be130dc2c649bed0b1c2f3da9c38e51358
RafaelWurzius <76011424+RafaelWurzius@users.noreply.github.com>
2023-04-27 09:08:16 -0300

    Update readme.mk

--- readme.mk
+++ readme.mk
@@ -1,5 +1,6 @@
 ENTREGAR 
 
 Colisão de circulos
-Esfera e um cubo 
+Esfera e um cubo(criar um cubo(de inox, q refelete bem) do lado da esfera, criar uma lampada meio em cima do cubo
+  e quando apagar a lampada do circulo e ligar do cubo, o cubo ficar da cor de cobre e quando desligar ficar cor de prata de novo)
 Escolher mais 3
