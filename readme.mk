41130af57813ee50b6248b0845cef9ea736f4d12
RafaelWurzius <76011424+RafaelWurzius@users.noreply.github.com>
2023-05-16 19:19:48 -0300

    Update readme.mk

--- readme.mk
+++ readme.mk
@@ -3,7 +3,7 @@ ENTREGAR
 Colisão de circulos
 Esfera e um cubo(criar um cubo(de inox, q refelete bem) do lado da esfera, criar uma lampada meio em cima do cubo
   e quando apagar a lampada do circulo e ligar do cubo, o cubo ficar da cor de cobre e quando desligar ficar cor de prata de novo)
-Escolher mais 3:
-  Animacao
-  Barquinho
-  Piramede-Cubo_3D ou push-pop(estático, utilização delas) ou 
+
+Animacao
+Barquinho
+Piramede-Cubo_3D ou push-pop(estático, utilização delas) ou PushPop2(mais completo)
