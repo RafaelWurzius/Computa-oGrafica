45714d46c9660e9f9c57bbe8602861383a02f7e7
RafaelWurzius <76011424+RafaelWurzius@users.noreply.github.com>
2023-06-01 09:41:39 -0300

    Update readme.mk

--- readme.mk
+++ readme.mk
@@ -2,9 +2,9 @@ TRABALHOS APRESENTADOS
 
 SeaQuest - SeaQuest.ccp
 
+Colisão de círculos - ColisaoCirculos.cpp
+Esfera e cubo de metal e cobre - EsferaCubo_Luz.cpp
+
 Animação do quadrado que bate nas laterais - Animacao.cpp
 Barquinho se movimentando - Barquinho.cpp
 Push e Pop - PushPop2.cpp
-
-Colisão de círculos - ColisaoCirculos.cpp
-Esfera e cubo de metal e cobre - EsferaCubo_Luz.cpp
